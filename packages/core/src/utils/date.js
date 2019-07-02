d41fffa4b8b2346f33d36a073a4d962627c7a81f
yucj <nakts0123@gmail.com>
2019-07-02 20:03:47 +0800

    Add jsDoc to utils/date in @twreporter/core

--- packages/core/src/utils/date.js
+++ packages/core/src/utils/date.js
@@ -1,5 +1,12 @@
-export function date2yyyymmdd(time, separator) {
-  const date = new Date(time)
+/**
+ *
+ * @export
+ * @param {number} timestampInMs - UNIX timestamp in mileseconds
+ * @param {string} separator - return string will be like 2019{separator}1{separator}31
+ * @returns
+ */
+export function date2yyyymmdd(timestampInMs, separator = '.') {
+  const date = new Date(timestampInMs)
   const year = date.getFullYear()
   const mon = date.getMonth() + 1
   const day = date.getDate()
