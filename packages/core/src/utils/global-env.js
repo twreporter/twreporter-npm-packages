9aa4c3259375a05301ff35728e9f532092c89723
yucj <nakts0123@gmail.com>
2019-07-04 18:14:56 +0800

    Fix global vars bug related to webpack defined plugin

--- packages/core/src/utils/global-env.js
+++ packages/core/src/utils/global-env.js
@@ -1,12 +1,14 @@
-import get from 'lodash/get'
-
-const _ = {
-  get,
-}
+/* 
+  Webpack defined plugin may replace the value of process.env.NODE_ENV directly.
+  So we cannot retireve the value via `_.get(process, 'env.NODE_ENV`)` since process.env is a empty object that the plugin won't touch.
+  Ref: https://stackoverflow.com/questions/41248575/webpack-react-process-env-always-empty-windows-10
+*/
+const isEnvExists =
+  typeof process === 'object' && typeof process.env === 'object'
 
 const globalEnv = {
-  nodeEnv: _.get(process, 'env.NODE_ENV') || 'development',
-  releaseBranch: _.get(process, 'env.RELEASE_BRANCH') || 'master',
+  nodeEnv: (isEnvExists && process.env.NODE_ENV) || 'development',
+  releaseBranch: (isEnvExists && process.env.RELEASE_BRANCH) || 'master',
 }
 
 export function getGlobalEnv() {
