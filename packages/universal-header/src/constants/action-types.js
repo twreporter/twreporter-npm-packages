40fb08c8df12b6b2a98cbe5890b41e7d181608b1
nickhsine <nickhsine97753017@gmail.com>
2020-02-13 15:24:58 +0800

    fix(universal-header): standalone header cannot work normally

---
+++ packages/universal-header/src/constants/action-types.js
@@ -0,0 +1,7 @@
+export default {
+  // auth
+  REQUEST_AUTH: 'request_authorization',
+  AUTH_SUCCESS: 'grant_authorization_success',
+  AUTH_FAILURE: 'grant_authorization_failure',
+  AUTH_CLEAR: 'clear_authorization',
+}
