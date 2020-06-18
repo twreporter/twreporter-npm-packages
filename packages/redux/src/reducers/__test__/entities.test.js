6f23b1aa22e06e324bc1bea8a766ce647688b834
nickhsine <nickhsine97753017@gmail.com>
2020-06-18 12:41:49 +0800

    test(redux): refactor reducers/__test__/entities.test.js
    - test case for returing initial state
    - test case for returing original state
    - test case for overwriting existed entities
    - test case for not overwriting existed entities

--- packages/redux/src/reducers/__test__/entities.test.js
+++ packages/redux/src/reducers/__test__/entities.test.js
@@ -4,114 +4,178 @@ import fieldNames from '../../constants/redux-state-field-names'
 import reducer from '../entities'
 import types from '../../constants/action-types'
 
-const post2 = {
+const metaPost1 = {
+  id: 'post-id-1',
+  slug: 'post-slug-1',
+  full: false,
+}
+
+const metaPost2 = {
   id: 'post-id-2',
   slug: 'post-slug-2',
   full: false,
 }
 
-const post3 = {
+const metaPost3 = {
   id: 'post-id-3',
   slug: 'post-slug-3',
   full: false,
 }
 
-const post4 = {
+const metaPost4 = {
   id: 'post-id-4',
   slug: 'post-slug-4',
   full: false,
 }
 
-const topic1 = {
+const metaTopic1 = {
   id: 'topic-id-1',
   slug: 'topic-slug-1',
-  relateds: [post3.id, post4.id],
-  full: true,
-}
-
-const topic2 = {
-  id: 'topic-id-2',
-  slug: 'topic-slug-2',
+  relateds: [metaPost1.id, metaPost3.id, metaPost4.id],
   full: false,
 }
 
-const post1 = {
-  id: 'post-id-1',
-  slug: 'post-slug-1',
-  relateds: [post2.id],
-  topics: topic1,
+const fullTopic1 = Object.assign({}, metaTopic1, {
   full: true,
+})
+
+const metaTopic2 = {
+  id: 'topic-id-2',
+  slug: 'topic-slug-2',
+  relateds: [],
+  full: false,
+}
+
+const fullPost1 = Object.assign({}, metaPost1, {
+  relateds: [metaPost2.id],
+  topics: metaTopic1,
+  full: true,
+})
+
+const initialState = {
+  [fieldNames.postsInEntities]: {
+    allIds: [],
+    byId: {},
+    slugToId: {},
+  },
+  [fieldNames.topicsInEntities]: {
+    allIds: [],
+    byId: {},
+    slugToId: {},
+  },
 }
 
 describe('entities reducer', () => {
   test('should return the initial state', () => {
-    expect(reducer({}, {})).toEqual({})
+    expect(reducer(undefined, {})).toEqual(initialState)
   })
 
-  test('should handle GET_CONTENT_FOR_INDEX_PAGE', () => {
-    expect(
-      reducer(
-        {
-          posts: {
-            byId: {
-              [post3.id]: post3,
-            },
-            slugToId: {
-              [post3.slug]: post3.id,
-            },
-            allIds: [post3.id],
-          },
-          topics: {
-            byId: {
-              [topic2.id]: topic2,
-            },
-            slugToId: {
-              [topic2.slug]: topic2.id,
-            },
-            allIds: [topic2.id],
-          },
+  test('should return the original state', () => {
+    const originalState = {
+      [fieldNames.postsInEntities]: {
+        allIds: [metaPost2.id],
+        byId: {
+          [metaPost2.id]: metaPost2,
         },
-        {
+        slugToId: {
+          [metaPost2.id]: metaPost2.slug,
+        },
+      },
+      [fieldNames.topicsInEntities]: {
+        allIds: [],
+        byId: {},
+        slugToId: {},
+      },
+    }
+
+    expect(reducer(originalState, {})).toEqual(originalState)
+  })
+
+  describe('should handle GET_CONTENT_FOR_INDEX_PAGE', () => {
+    test('when state is initialState', () => {
+      expect(
+        reducer(initialState, {
           type: types.GET_CONTENT_FOR_INDEX_PAGE,
           payload: {
             items: {
-              [fieldNames.sections.latestSection]: [post2],
-              [fieldNames.sections.editorPicksSection]: [post2],
-              [fieldNames.sections.reviewsSection]: [post3],
-              [fieldNames.sections.latestTopicSection]: [topic1],
-              [fieldNames.sections.topicsSection]: [topic2],
-              [fieldNames.sections.photosSection]: [post2],
-              [fieldNames.sections.infographicsSection]: [post3],
-              [fieldNames.categories.humanRightsAndSociety]: [post4],
+              [fieldNames.sections.latestSection]: [metaPost1],
+              [fieldNames.sections.editorPicksSection]: [metaPost2],
+              [fieldNames.sections.reviewsSection]: [metaPost3],
+              [fieldNames.sections.latestTopicSection]: [metaTopic1],
+              [fieldNames.sections.topicsSection]: [metaTopic2],
+              [fieldNames.sections.photosSection]: [metaPost4],
+              [fieldNames.sections.infographicsSection]: [],
+              [fieldNames.categories.humanRightsAndSociety]: [],
             },
           },
-        }
-      )
-    ).toEqual({
-      posts: {
-        byId: {
-          [post2.id]: post2,
-          [post3.id]: post3,
-          [post4.id]: post4,
+        })
+      ).toEqual({
+        posts: {
+          byId: {
+            [metaPost1.id]: metaPost1,
+            [metaPost2.id]: metaPost2,
+            [metaPost3.id]: metaPost3,
+            [metaPost4.id]: metaPost4,
+          },
+          slugToId: {
+            [metaPost1.slug]: metaPost1.id,
+            [metaPost2.slug]: metaPost2.id,
+            [metaPost3.slug]: metaPost3.id,
+            [metaPost4.slug]: metaPost4.id,
+          },
+          allIds: expect.arrayContaining([
+            metaPost1.id,
+            metaPost2.id,
+            metaPost3.id,
+            metaPost4.id,
+          ]),
         },
-        slugToId: {
-          [post2.slug]: post2.id,
-          [post3.slug]: post3.id,
-          [post4.slug]: post4.id,
+        topics: {
+          byId: {
+            [metaTopic1.id]: metaTopic1,
+            [metaTopic2.id]: metaTopic2,
+          },
+          slugToId: {
+            [metaTopic1.slug]: metaTopic1.id,
+            [metaTopic2.slug]: metaTopic2.id,
+          },
+          allIds: [metaTopic1.id, metaTopic2.id],
         },
-        allIds: [post3.id, post2.id, post4.id],
-      },
-      topics: {
-        byId: {
-          [topic1.id]: topic1,
-          [topic2.id]: topic2,
+      })
+    })
+
+    test('without overwriting existed entities', () => {
+      const state = {
+        [fieldNames.postsInEntities]: {
+          byId: {
+            [fullPost1.id]: fullPost1,
+          },
+          slugToId: {
+            [fullPost1.slug]: fullPost1.id,
+          },
+          allIds: [fullPost1.id],
         },
-        slugToId: {
-          [topic1.slug]: topic1.id,
-          [topic2.slug]: topic2.id,
+        [fieldNames.topicsInEntities]: {
+          byId: {
+            [fullTopic1.id]: fullTopic1,
+          },
+          slugToId: {
+            [fullTopic1.slug]: fullTopic1.id,
+          },
+          allIds: [fullTopic1.id],
         },
-        allIds: [topic2.id, topic1.id],
-      },
+      }
+      expect(
+        reducer(state, {
+          type: types.GET_CONTENT_FOR_INDEX_PAGE,
+          payload: {
+            items: {
+              [fieldNames.sections.latestSection]: [metaPost1],
+              [fieldNames.sections.latestTopicSection]: [metaTopic1],
+            },
+          },
+        })
+      ).toEqual(state)
     })
   })
 
@@ -122,19 +186,19 @@ describe('entities reducer', () => {
         {
           type: types.GET_TOPICS_FOR_INDEX_PAGE,
           payload: {
-            items: [topic2],
+            items: [metaTopic2],
           },
         }
       )
     ).toEqual({
       topics: {
         byId: {
-          [topic2.id]: topic2,
+          [metaTopic2.id]: metaTopic2,
         },
         slugToId: {
-          [topic2.slug]: topic2.id,
+          [metaTopic2.slug]: metaTopic2.id,
         },
-        allIds: [topic2.id],
+        allIds: [metaTopic2.id],
       },
     })
   })
@@ -146,19 +210,19 @@ describe('entities reducer', () => {
         {
           type: types.GET_EDITOR_PICKED_POSTS,
           payload: {
-            items: [post2],
+            items: [metaPost2],
           },
         }
       )
     ).toEqual({
       posts: {
         byId: {
-          [post2.id]: post2,
+          [metaPost2.id]: metaPost2,
         },
         slugToId: {
-          [post2.slug]: post2.id,
+          [metaPost2.slug]: metaPost2.id,
         },
-        allIds: [post2.id],
+        allIds: [metaPost2.id],
       },
     })
   })
@@ -170,19 +234,19 @@ describe('entities reducer', () => {
         {
           type: types.GET_PHOTOGRAPHY_POSTS_FOR_INDEX_PAGE,
           payload: {
-            items: [post2],
+            items: [metaPost2],
           },
         }
       )
     ).toEqual({
       posts: {
         byId: {
-          [post2.id]: post2,
+          [metaPost2.id]: metaPost2,
         },
         slugToId: {
-          [post2.slug]: post2.id,
+          [metaPost2.slug]: metaPost2.id,
         },
-        allIds: [post2.id],
+        allIds: [metaPost2.id],
       },
     })
   })
@@ -194,44 +258,46 @@ describe('entities reducer', () => {
         {
           type: types.GET_INFOGRAPHIC_POSTS_FOR_INDEX_PAGE,
           payload: {
-            items: [post2],
+            items: [metaPost2],
           },
         }
       )
     ).toEqual({
       posts: {
         byId: {
-          [post2.id]: post2,
+          [metaPost2.id]: metaPost2,
         },
         slugToId: {
-          [post2.slug]: post2.id,
+          [metaPost2.slug]: metaPost2.id,
         },
-        allIds: [post2.id],
+        allIds: [metaPost2.id],
       },
     })
   })
 
-  test('should handle GET_LISTED_POSTS', () => {
-    expect(
-      reducer(
-        {},
-        {
-          type: types.GET_LISTED_POSTS,
-          payload: {
-            items: [post2],
+  describe('should handle GET_LISTED_POSTS', () => {
+    test('', () => {
+      expect(
+        reducer(
+          {},
+          {
+            type: types.GET_LISTED_POSTS,
+            payload: {
+              items: [metaPost2],
+            },
+          }
+        )
+      ).toEqual({
+        posts: {
+          byId: {
+            [metaPost2.id]: metaPost2,
           },
-        }
-      )
-    ).toEqual({
-      posts: {
-        byId: {
-          [post2.id]: post2,
+          slugToId: {
+            [metaPost2.slug]: metaPost2.id,
+          },
+          allIds: [metaPost2.id],
         },
-        slugToId: {
-          [post2.slug]: post2.id,
-        },
-        allIds: [post2.id],
-      },
+      })
     })
   })
 
@@ -241,32 +307,30 @@ describe('entities reducer', () => {
         {
           posts: {
             byId: {
-              [post1.id]: post1,
+              [metaPost1.id]: metaPost1,
             },
             slugToId: {
-              [post1.slug]: post1.id,
+              [metaPost1.slug]: metaPost1.id,
             },
-            allIds: [post1.id],
+            allIds: [metaPost1.id],
           },
         },
         {
           type: types.GET_A_FULL_POST,
           payload: {
-            post: post2,
+            post: fullPost1,
           },
         }
       )
     ).toEqual({
       posts: {
         byId: {
-          [post1.id]: post1,
-          [post2.id]: post2,
+          [fullPost1.id]: fullPost1,
         },
         slugToId: {
-          [post1.slug]: post1.id,
-          [post2.slug]: post2.id,
+          [fullPost1.slug]: fullPost1.id,
         },
-        allIds: [post1.id, post2.id],
+        allIds: [fullPost1.id],
       },
     })
   })
@@ -277,32 +341,30 @@ describe('entities reducer', () => {
         {
           topics: {
             byId: {
-              [topic1.id]: topic1,
+              [metaTopic1.id]: metaTopic1,
             },
             slugToId: {
-              [topic1.slug]: topic1.id,
+              [metaTopic1.slug]: metaTopic1.id,
             },
-            allIds: [topic1.id],
+            allIds: [metaTopic1.id],
           },
         },
         {
           type: types.GET_A_FULL_TOPIC,
           payload: {
-            topic: topic2,
+            topic: fullTopic1,
           },
         }
       )
     ).toEqual({
       topics: {
         byId: {
-          [topic1.id]: topic1,
-          [topic2.id]: topic2,
+          [fullTopic1.id]: fullTopic1,
         },
         slugToId: {
-          [topic1.slug]: topic1.id,
-          [topic2.slug]: topic2.id,
+          [fullTopic1.slug]: fullTopic1.id,
         },
-        allIds: [topic1.id, topic2.id],
+        allIds: [fullTopic1.id],
       },
     })
   })
