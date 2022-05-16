7148d4295aaf4b76098c5130a80d29d742b46906
Aylie Chou <aylie_chou@twreporter.org>
2022-05-16 16:37:40 +0800

    fix: fix defect & pill button style
    - use old bookmark add icon
    - fix bookmark style
    - fix pill button style

--- packages/react-components/src/bookmark-list/empty-guide.js
+++ packages/react-components/src/bookmark-list/empty-guide.js
@@ -6,7 +6,7 @@ import predefinedPropTypes from '@twreporter/core/lib/constants/prop-types'
 import releaseBranchConsts from '@twreporter/core/lib/constants/release-branch'
 import requestOrigin from '@twreporter/core/lib/constants/request-origins'
 // components
-import { Bookmark } from '../icon'
+import BookmarkIcon from './assets/bookmark-add.svg'
 import { P1, P2 } from '../text/paragraph'
 import { PillButton } from '../button'
 
@@ -23,6 +23,7 @@ const TextContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  color: ${colorGrayscale.gray800};
 `
 
 const ButtonContainer = styled.a`
@@ -31,11 +32,11 @@ const ButtonContainer = styled.a`
 
 const GuideContainer = styled.div`
   display: flex;
+  align-items: center;
   color: ${colorGrayscale.gray600};
-  svg {
-    background-color: ${colorGrayscale.gray600};
+  img {
     width: 18px;
-    height: 18px;
+    height: 16px;
     margin: 0 4px;
   }
 `
@@ -50,7 +51,7 @@ const EmptyGuide = ({ releaseBranch = releaseBranchConsts.master }) => {
         <P1 text="你還沒有儲存任何文章！" weight="bold" />
         <GuideContainer>
           <P2 text="點擊" />
-          <Bookmark type="add" releaseBranch={releaseBranch} />
+          <img src={BookmarkIcon} alt="Add Bookmark" />
           <P2 text="將喜愛的文章加入我的書籤" />
         </GuideContainer>
       </TextContainer>
