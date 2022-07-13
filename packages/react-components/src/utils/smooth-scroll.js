89dfb8c9facde8dc41a3dbe462553ab1be7ad85b
duidae <duidae@gmail.com>
2022-07-14 00:06:42 +0800

    fix: inline tool functions of smooth scroll

--- packages/react-components/src/utils/smooth-scroll.js
+++ packages/react-components/src/utils/smooth-scroll.js
@@ -3,24 +3,24 @@
 // apply to all anchors, so copy the implementation of smoothScroll lib in npm:
 // ref: https://github.com/alicelieutier/smoothScroll/blob/master/smoothscroll.js
 
-const position = function(start, end, elapsed, duration) {
-  if (elapsed > duration) return end
-  return start + (end - start) * easeInOutCubic(elapsed / duration) // <-- you can change the easing funtion there
-  // return start + (end - start) * (elapsed / duration) // <-- this would give a linear scroll
-}
-
-const getTop = function(element, start) {
-  // return value of html.getBoundingClientRect().top ... IE : 0, other browsers : -pageYOffset
-  if (element.nodeName === 'HTML') return -start
-  return element.getBoundingClientRect().top + start
-}
-
-const easeInOutCubic = function(t) {
-  return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1
-}
-
 const defaultDuration = 500
 export default function(el, duration, callback, context) {
+  const position = function(start, end, elapsed, duration) {
+    if (elapsed > duration) return end
+    return start + (end - start) * easeInOutCubic(elapsed / duration) // <-- you can change the easing funtion there
+    // return start + (end - start) * (elapsed / duration) // <-- this would give a linear scroll
+  }
+
+  const getTop = function(element, start) {
+    // return value of html.getBoundingClientRect().top ... IE : 0, other browsers : -pageYOffset
+    if (element.nodeName === 'HTML') return -start
+    return element.getBoundingClientRect().top + start
+  }
+
+  const easeInOutCubic = function(t) {
+    return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1
+  }
+
   duration = duration || defaultDuration
   context = context || window
   var start = context.scrollTop || window.pageYOffset
@@ -55,9 +55,3 @@ export default function(el, duration, callback, context) {
   }
   step()
 }
-
-/*
-e.preventDefault()
-const id = e?.target?.hash?.substring(1)
-const element = document.getElementById(id)
-*/
