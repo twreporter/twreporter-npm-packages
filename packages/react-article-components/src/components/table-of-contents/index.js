de6aba4f8592991c0c384b6ad11d9627c27c7fe0
duidae <duidae@gmail.com>
2022-07-22 15:53:38 +0800

    Revert "fix: import anchor scroll duration to toc"
    This reverts commit 4c13b4f72e3fc01ce6a2de98ea37c92de6cedd4e.

--- packages/react-article-components/src/components/table-of-contents/index.js
+++ packages/react-article-components/src/components/table-of-contents/index.js
@@ -4,7 +4,10 @@ import React from 'react'
 import TOC from '@twreporter/react-components/lib/table-of-contents'
 import map from 'lodash/map'
 import Tab from '../../assets/table-of-contents/long-form-tab.svg'
-import { WAIT_AFTER_REACH_ANCHOR } from '../../constants/anchor'
+import {
+  ANCHOR_SCROLL_DURATION,
+  WAIT_AFTER_REACH_ANCHOR,
+} from '../../constants/anchor'
 
 const _ = {
   map,
@@ -83,6 +86,7 @@ class TableOfContents extends React.PureComponent {
         <TOC.React.TableOfContents
           className={className}
           manager={manager}
+          scrollDuration={ANCHOR_SCROLL_DURATION}
           render={(anchors, highlightAnchor, handleAnchorClick) => {
             const anchorsJSX = _.map(anchors, anchor => {
               const toHighlight = anchor === highlightAnchor
