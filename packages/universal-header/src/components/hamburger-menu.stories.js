fc4c76bf98534d7b2f3fdf7695ae75adec1df510
Aylie Chou <aylie_chou@twreporter.org>
2022-10-17 01:29:37 +0800

    feat: add hamburger-menu component with story & feature toggle

---
+++ packages/universal-header/src/components/hamburger-menu.stories.js
@@ -0,0 +1,38 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import HamburgerMenu from './hamburger-menu'
+import HeaderContext from '../contexts/header-context'
+import { THEME_STORYBOOK_ARG_TYPE } from '@twreporter/core/lib/constants/theme'
+import { BRANCH_STORYBOOK_ARG_TYPE } from '@twreporter/core/lib/constants/release-branch'
+
+export default {
+  title: 'Hamburger/Menu',
+  component: HamburgerMenu,
+  argTypes: {
+    theme: THEME_STORYBOOK_ARG_TYPE,
+    releaseBranch: BRANCH_STORYBOOK_ARG_TYPE,
+  },
+}
+
+const defaultActions = [{ key: 'newsLetter' }, { key: 'support' }]
+const onClose = () => window?.alert('click close !')
+export const menu = props => {
+  const { theme, releaseBranch } = props
+  const context = {
+    theme,
+    releaseBranch,
+    isLinkExternal: true,
+  }
+  return (
+    <HeaderContext.Provider value={context}>
+      <HamburgerMenu {...props} />
+    </HeaderContext.Provider>
+  )
+}
+menu.args = {
+  actions: defaultActions,
+  handleClose: onClose,
+}
+menu.parameter = {
+  controls: { exclude: ['actions', 'handleClose'] },
+}
