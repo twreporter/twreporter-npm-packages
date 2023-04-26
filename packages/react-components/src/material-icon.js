1937a3fbf7841aad7176d644cc3c9bb77575fb64
Aylie Chou <aylie_chou@twreporter.org>
2023-04-26 18:29:51 +0800

    fix: use `material-symbols` package instead of `react-material-symbols`
    - sb6 support only es2016, while `react-material-symbols` use es2020 syntax
    - `react-material-symbols` has dependency to use react ^18
      - we use v16 currently

--- packages/react-components/src/material-icon.js
+++ packages/react-components/src/material-icon.js
@@ -1,4 +1,43 @@
-import { MaterialSymbol } from 'react-material-symbols'
-import 'react-material-symbols/dist/outlined.css'
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import 'material-symbols/outlined.css'
 
-export default MaterialSymbol
+const StyledSymbol = styled.span`
+  font-variation-settings: ${(props) => `
+    'FILL': ${props.fill},
+    'wght': ${props.weight},
+    'GRAD': ${props.grade},
+    'opsz': ${props.size}
+  `};
+`
+
+const MeterialSymbol = ({
+  icon,
+  fill = false,
+  weight = 400,
+  grade = 0,
+  size = 24,
+}) => {
+  const className = 'material-symbols-outlined'
+  return (
+    <StyledSymbol
+      className={className}
+      fill={fill ? '0' : '1'}
+      weight={weight}
+      grade={grade}
+      size={size}
+    >
+      {icon}
+    </StyledSymbol>
+  )
+}
+MeterialSymbol.propTypes = {
+  icon: PropTypes.string.isRequired,
+  fill: PropTypes.bool,
+  weight: PropTypes.number,
+  grade: PropTypes.number,
+  size: PropTypes.number,
+}
+
+export default MeterialSymbol
