2f97f82a2d754c462dbbfef8a3e0ff9e9db0435c
Lucien <liruchen0302@gmail.com>
2023-06-07 13:28:50 +0800

    feat: add email subscription options

---
+++ packages/core/src/constants/email-subscription-options.js
@@ -0,0 +1,17 @@
+// featured: 2f1c91a75a # 報導者精選
+// behind_the_scenes: 219df4131b # 採訪幕後故事
+// operational_journal: ca9d491549 # 報導者營運手記
+// event_updates: 0345c43d67 # 活動資訊與最新動態
+export const OPTION_KEYS = {
+  featured: 'featured',
+  behindTheScenes: 'behind_the_scenes',
+  operationalJournal: 'operational_journal',
+  eventUpdates: 'event_updates',
+}
+
+export const OPTION_KEY_ID = {
+  [OPTION_KEYS.featured]: '2f1c91a75a',
+  [OPTION_KEYS.behindTheScenes]: '219df4131b',
+  [OPTION_KEYS.operationalJournal]: 'ca9d491549',
+  [OPTION_KEYS.eventUpdates]: '0345c43d67',
+}
