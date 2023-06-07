3d1119766205a566d504452e3325e260b395d63f
Lucien <liruchen0302@gmail.com>
2023-06-07 17:15:38 +0800

    feat: add read preference

--- packages/core/src/constants/email-subscription.js
+++ packages/core/src/constants/email-subscription.js
@@ -2,16 +2,16 @@
 // behind_the_scenes: 219df4131b # 採訪幕後故事
 // operational_journal: ca9d491549 # 報導者營運手記
 // event_updates: 0345c43d67 # 活動資訊與最新動態
-export const OPTION_KEYS = {
+export const EMAIL_SUBSCRIPTION_KEY = {
   featured: 'featured',
   behindTheScenes: 'behind_the_scenes',
   operationalJournal: 'operational_journal',
   eventUpdates: 'event_updates',
 }
 
-export const OPTION_KEY_ID = {
-  [OPTION_KEYS.featured]: '2f1c91a75a',
-  [OPTION_KEYS.behindTheScenes]: '219df4131b',
-  [OPTION_KEYS.operationalJournal]: 'ca9d491549',
-  [OPTION_KEYS.eventUpdates]: '0345c43d67',
+export const EMAIL_SUBSCRIPTION_KEY_ID = {
+  [EMAIL_SUBSCRIPTION_KEY.featured]: '2f1c91a75a',
+  [EMAIL_SUBSCRIPTION_KEY.behindTheScenes]: '219df4131b',
+  [EMAIL_SUBSCRIPTION_KEY.operationalJournal]: 'ca9d491549',
+  [EMAIL_SUBSCRIPTION_KEY.eventUpdates]: '0345c43d67',
 }
