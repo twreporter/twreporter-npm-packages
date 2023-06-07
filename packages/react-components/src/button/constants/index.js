9a661de3ce68953c083e8677e39c0b87887e1749
Aylie Chou <aylie_chou@twreporter.org>
2023-06-07 17:31:07 +0800

    fix: deprecate TEXT_BUTTON_THEME_PROP_TYPES

--- packages/react-components/src/button/constants/index.js
+++
@@ -1,6 +0,0 @@
-import PropTypes from 'prop-types'
-import { TEXT_BUTTON_THEME } from '@twreporter/core/lib/constants/theme'
-
-export const TEXT_BUTTON_THEME_PROP_TYPES = PropTypes.oneOf(
-  Object.values(TEXT_BUTTON_THEME)
-)
