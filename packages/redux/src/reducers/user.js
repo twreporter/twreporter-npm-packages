bff225bfeb16460b35851ca47712cd488e059414
Lucien <liruchen0302@gmail.com>
2023-06-21 17:57:33 +0800

    feat: getUserData from api

--- packages/redux/src/reducers/user.js
+++ packages/redux/src/reducers/user.js
@@ -1,7 +1,5 @@
 import types from '../constants/action-types'
 
-// import { MEMBER_ROLE } from '@twreporter/core/lib/constants/member-role'
-
 // lodash
 import get from 'lodash/get'
 import merge from 'lodash/merge'
@@ -15,13 +13,13 @@ const initState = {
   error: null,
   isFetching: false,
   isReady: false,
-  // TODO: need add other user state after connect to getUser api
-  // userID: -1,
-  // firstName: '',
-  // lastName: '',
-  // role: MEMBER_ROLE.explorer,
-  // email: '',
-  // activated: false,
+  userID: -1,
+  email: '',
+  firstName: '',
+  lastName: '',
+  roles: [],
+  registrationDate: '',
+  activated: false,
   readPreference: [],
   maillist: [],
 }
@@ -29,6 +27,51 @@ const initState = {
 export default function user(state = initState, action) {
   const { payload } = action
   switch (action.type) {
+    case types.user.read.request: {
+      return _.merge({}, state, {
+        isFetching: true,
+        isReady: false,
+      })
+    }
+
+    case types.user.read.success: {
+      const userID = _.get(payload, 'data.data.user_id')
+      const email = _.get(payload, 'data.data.email')
+      const firstName = _.get(payload, 'data.data.first_name')
+      const lastName = _.get(payload, 'data.data.last_name')
+      const roles = _.get(payload, 'data.data.roles')
+      const registrationDate = _.get(payload, 'data.data.registration_date')
+      const activated = _.get(payload, 'data.data.activated')
+      const readPreference = _.get(payload, 'data.data.read_preference')
+      const maillist = _.get(payload, 'data.data.maillist')
+      state.readPreference = readPreference
+      state.maillist = maillist
+      return _.merge({}, state, {
+        ...state,
+        error: null,
+        isFetching: false,
+        isReady: true,
+        userID,
+        email,
+        firstName,
+        lastName,
+        roles,
+        registrationDate,
+        activated,
+        readPreference,
+        maillist,
+      })
+    }
+
+    case types.user.read.failure: {
+      return {
+        ...state,
+        error: payload.error,
+        isFetching: false,
+        isReady: false,
+      }
+    }
+
     case types.user.update.request: {
       return _.merge({}, state, {
         isFetching: true,
