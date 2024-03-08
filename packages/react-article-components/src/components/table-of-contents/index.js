0557526f7cb169e7fcad44cb40c8de4a024e7cf8
Lucien <liruchen0302@gmail.com>
2024-03-08 15:04:36 +0800

    fix: remove unused code & fix comments

--- packages/react-article-components/src/components/table-of-contents/index.js
+++ packages/react-article-components/src/components/table-of-contents/index.js
@@ -54,46 +54,7 @@ const TableOfContents = ({
         capture: true,
       })
     }
-  }, [isExpanded])
-
-  const [scrollDirection, setScrollDirection] = useState('init')
-  useEffect(() => {
-    const threshold = 16
-    let lastScrollY = window.pageYOffset
-    let ticking = false
-
-    const updateScrollDirection = () => {
-      const scrollY = window.pageYOffset
-      if (_.isNil(lastScrollY)) {
-        ticking = false
-        return
-      }
-
-      if (Math.abs(scrollY - lastScrollY) < threshold) {
-        ticking = false
-        return
-      }
-      const scrollDirection = scrollY > lastScrollY ? 'down' : 'up'
-      setScrollDirection(scrollDirection)
-      lastScrollY = scrollY > 0 ? scrollY : 0
-      ticking = false
-    }
-
-    const onScroll = () => {
-      if (!ticking) {
-        window.requestAnimationFrame(updateScrollDirection)
-        ticking = true
-      }
-    }
-
-    window.addEventListener('scroll', onScroll)
-
-    return () => {
-      window.removeEventListener('scroll', onScroll)
-      lastScrollY = null
-      ticking = null
-    }
-  }, [scrollDirection])
+  }, [])
 
   useEffect(() => {
     onToggleTabExpanded(isExpanded)
