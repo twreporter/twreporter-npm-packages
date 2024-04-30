5887d577a2509214f3e00dd22d404c506e517ab6
Lucien <liruchen0302@gmail.com>
2024-04-30 22:03:39 +0800

    feat: logo footer

--- packages/react-components/src/logo/components/logo-footer.js
+++ packages/react-components/src/logo/components/logo-footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
 // utils
 import pathUtil from '../utils/path'
 // @twreporter
@@ -7,26 +8,40 @@ import {
   BRANCH,
   BRANCH_PROP_TYPES,
 } from '@twreporter/core/lib/constants/release-branch'
+import mq from '@twreporter/core/lib/utils/media-query'
 
 const LogoContainer = styled.img`
-  filter: grayscale(100%);
-  opacity: 0.4;
-
+  filter: none;
+  opacity: 1;
+  width: 272px;
+  height: 29px;
   &:hover {
-    filter: none;
-    opacity: 1;
+    src: ${props => `${props.src}`};
   }
+  ${mq.tabletAndBelow`
+    width: 232px;
+    height: 25px;
+  `}
 `
 
-const LogoFooter = ({ releaseBranch }) => {
-  const logoSrc = pathUtil.selectLogoPath('footer', releaseBranch)
-  return <LogoContainer alt="The Reporter Logo" src={logoSrc} />
+const logoType = {
+  DEFAULT: 'default',
+  WHITE: 'white',
+}
+
+const LogoFooter = ({ releaseBranch, type }) => {
+  const src = pathUtil.selectLogoPath('footer', releaseBranch, type)
+  return <LogoContainer alt="The Reporter Logo" src={src} />
 }
 LogoFooter.propTypes = {
   releaseBranch: BRANCH_PROP_TYPES,
+  type: PropTypes.oneOf(Object.values(logoType)),
 }
 LogoFooter.defaultProps = {
   releaseBranch: BRANCH.master,
+  type: logoType.DEFAULT,
 }
+
+LogoFooter.Type = logoType
 
 export default LogoFooter
