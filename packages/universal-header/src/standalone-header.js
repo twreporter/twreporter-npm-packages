9dc719e1929ff2627d4d55ae285eb9ae00896769
Lucien <liruchen0302@gmail.com>
2024-06-13 18:26:39 +0800

    chore(universal-header): update redux to latest version

--- packages/universal-header/src/standalone-header.js
+++ packages/universal-header/src/standalone-header.js
@@ -3,10 +3,9 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import requestOrigins from '@twreporter/core/lib/constants/request-origins'
 import rootReducer from './reducers/index'
-import thunk from 'redux-thunk'
 import { CONTEXT_PROP } from './constants/prop-types'
 import { Provider } from 'react-redux'
-import { applyMiddleware, createStore } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import { getAccessToken } from './actions/auth'
 
 export default class StandaloneHeader extends React.PureComponent {
@@ -27,18 +26,17 @@ export default class StandaloneHeader extends React.PureComponent {
   constructor(props) {
     super(props)
     const { releaseBranch } = this.props
-    this.store = createStore(
-      rootReducer,
-      {
+    this.store = configureStore({
+      reducer: {
+        rootReducer,
         origins: requestOrigins.forClientSideRendering[releaseBranch],
         auth: {},
       },
-      applyMiddleware(thunk)
-    )
+    })
   }
 
   componentDidMount() {
-    this.store.dispatch(getAccessToken()).catch(failAction => {
+    this.store.dispatch(getAccessToken()).catch((failAction) => {
       console.log(failAction)
     })
   }
