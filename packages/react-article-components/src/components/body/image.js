9c8d5a4f6948ec948232fe402b70fde05ebbde42
Lucien <liruchen0302@gmail.com>
2024-06-21 14:37:23 +0800

    feat: prop for tracking section

--- packages/react-article-components/src/components/body/image.js
+++ packages/react-article-components/src/components/body/image.js
@@ -1,8 +1,15 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+// @twreporter
+import { P2 } from '@twreporter/react-components/lib/text/paragraph'
+import { colorGrayscale } from '@twreporter/core/lib/constants/color'
+// components
 import Img from '../img-with-placeholder'
 import Multimedia from './multimedia'
-import PropTypes from 'prop-types'
+// constants
 import predefinedPropTypes from '../../constants/prop-types/body'
-import React, { PureComponent } from 'react'
+// lodash
 import get from 'lodash/get'
 
 const _ = {
@@ -16,24 +23,32 @@ const imgProps = {
 const Container = Multimedia.Block
 const Caption = Multimedia.Caption
 
+const CaptionForTrackingSection = styled(P2)`
+  color: ${colorGrayscale.gray700};
+  margin-top: 8px;
+`
+
 export default class Image extends PureComponent {
   static propTypes = {
     className: PropTypes.string,
     data: predefinedPropTypes.elementData.isRequired,
     small: PropTypes.bool,
+    forTrackingSection: PropTypes.bool,
   }
 
   static defaultProps = {
     className: '',
     small: false,
+    forTrackingSection: false,
   }
 
   render() {
-    const { className, data, small } = this.props
+    const { className, data, small, forTrackingSection } = this.props
     const image = _.get(data, ['content', 0])
     const caption = _.get(image, 'description')
     const alt = _.get(image, 'keywords', caption)
     const appendedClassName = className + ' avoid-break'
+
     return (
       <Container className={appendedClassName} $small={small}>
         <figure itemScope itemType="http://schema.org/ImageObject">
@@ -45,7 +60,13 @@ export default class Image extends PureComponent {
             clickable={true}
             /* TODO: add sizes */
           />
-          {caption ? <Caption itemprop="description">{caption}</Caption> : null}
+          {caption ? (
+            forTrackingSection ? (
+              <CaptionForTrackingSection text={caption} />
+            ) : (
+              <Caption itemprop="description">{caption}</Caption>
+            )
+          ) : null}
         </figure>
       </Container>
     )
