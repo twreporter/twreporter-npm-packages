17ff1dd94ee4c725969465dd1ae2449b24112da6
Lucien <liruchen0302@gmail.com>
2025-12-22 10:52:51 +0800

    fix(core): remove extra space

--- packages/core/src/constants/fundraising.js
+++ packages/core/src/constants/fundraising.js
@@ -1,4 +1,4 @@
-const fundraisingId = '衛部救字第 1141363741  號｜勸募期間 2026/1/1~12/31'
+const fundraisingId = '衛部救字第 1141363741 號｜勸募期間 2026/1/1~12/31'
 
 export default fundraisingId
 export { fundraisingId }
